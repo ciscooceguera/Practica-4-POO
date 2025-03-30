6473378a7697a2904a20ee6afcbbea07beb9ed5f
ciscooceguera <oceguera.14@hotmail.com>
2025-03-30 14:35:10 -0700

    Agregue metodos y atributos de las clases

--- src/Dado.java
+++ src/Dado.java
@@ -1,10 +1,13 @@
 import java.util.Random;
-
+// clase dado se encargara de toda accion que implique a un dado
 public class Dado{
     private int valor;
+    // constructor
     public Dado(){
+        // inicializo valor en 0
         this.valor = 0;
     }
+    // genera un numero random y lo guarda en el atributo
     public int tirarDado(){
         Random rnd = new Random();
         this.valor = rnd.nextInt(6)+1;
--- src/Farkle.java
+++ src/Farkle.java
@@ -1,3 +1,22 @@
+import java.util.ArrayList;
+// clase Farkle se encarga del flujo del juego
 public class Farkle {
+    // atributos
+    private ArrayList<Dado> dados;
+    private int turno, puntajeSkip, puntajeWin, numJugadores;
+    // constructor
+    public Farkle(int numJugadores, int puntajeSkip, int puntajeWin){
+        this.numJugadores = numJugadores;
+        this.puntajeSkip = puntajeSkip;
+        this.puntajeWin = puntajeWin;
+        dados = new ArrayList<>();
+    }
+    // metodo para iniciar el juego
+    public void comenzarJuego(){
 
+    }
+    // metodo para cambiar el turno
+    public void cambioDeTurno(){
+
+    }
 }
