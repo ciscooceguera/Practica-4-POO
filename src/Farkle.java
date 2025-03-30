ed94ad0475c8caff36116daa9965cb2d89b45b23
ciscooceguera <oceguera.14@hotmail.com>
2025-03-30 14:47:15 -0700

    A

--- src/Farkle.java
+++ src/Farkle.java
@@ -10,6 +10,7 @@ public class Farkle {
         this.puntajeSkip = puntajeSkip;
         this.puntajeWin = puntajeWin;
         dados = new ArrayList<>();
+        turno = 0;
     }
     // metodo para iniciar el juego
     public void comenzarJuego(){
@@ -18,5 +19,27 @@ public class Farkle {
     // metodo para cambiar el turno
     public void cambioDeTurno(){
 
+    }
+    // metodo para tirarLos6Dados
+    public void tirarDados(){
+
+    }
+    // metodo para preguntar si desea parar
+    public void decidirSiParar(){
+
+    }
+    // metodo para evaluar si el jugador en turno puede parar
+    public boolean evaluarSiJugadorPuedeParar(){
+        return false;
+    }
+    // metodo para verificar si el jugador hizo un farkled, es decir; no puntuó de ninguna manera,
+    // por lo que el puntaje que haya acumulado en su turno se pierde
+    public boolean verificarFarkled(){
+        return false;
+    }
+    // metodo para verificar si el jugador obtuvo dados calientes, es decir que el jugador haya logrado,
+    // puntuar con los 6 dados en un turno
+    public boolean verificarHotDice(){
+        return false;
     }
 }
