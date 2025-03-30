11ccaeed6ed24406c5500d59387f50907587b04a
ciscooceguera <oceguera.14@hotmail.com>
2025-03-30 14:10:20 -0700

    Anadi la clase jugador

--- src/Dado.java
+++ src/Dado.java
@@ -1,3 +1,13 @@
-public class Dado{
+import java.util.Random;
 
+public class Dado{
+    private int valor;
+    public Dado(){
+        this.valor = 0;
+    }
+    public int tirarDado(){
+        Random rnd = new Random();
+        this.valor = rnd.nextInt(6)+1;
+        return this.valor;
+    }
 }
