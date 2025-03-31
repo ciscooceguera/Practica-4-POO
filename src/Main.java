f2ec2e78164c9f08b0e77ada04330b6f9e243594
ciscooceguera <oceguera.14@hotmail.com>
2025-03-30 19:21:16 -0700

    Actualizo cambios realizados en el flujo del juego

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,33 @@ public class Main {
         // inicializo la bandera para el control del juego
         int yesNoJugarResponse = 0;
         // ciclo while para que se pueda jugar siempre que asi se desee
-        while (yesNoJugarResponse == 0){
+        while (yesNoJugarResponse != 2){
+            // inicializo objeto de botones
+            Object[] botones = {"Jugar","Creditos","Salir"};
             // pregunto si quiere jugar
-            yesNoJugarResponse = JOptionPane.showConfirmDialog(null,"Quieres jugar?:","",JOptionPane.YES_NO_OPTION);
+            yesNoJugarResponse = JOptionPane.showOptionDialog(null,
+                    "Seleccione la opcion.",
+                    "Pregunta",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.QUESTION_MESSAGE,
+                    null,
+                    botones,
+                    botones[0]);
             // switch
             switch(yesNoJugarResponse){
                 // caso SI quiere jugar
                 case 0:
                     // solicito el numero de jugadores
-                    String numJugadoresStr = JOptionPane.showInputDialog("Ingrese numero de jugadores: ");
-                    int numJugadores = Integer.parseInt(numJugadoresStr);
+                    int numJugadores = 0;
+                    while (numJugadores<2) {
+                        String numJugadoresStr = JOptionPane.showInputDialog("Ingrese numero de jugadores: ");
+                        numJugadores = Integer.parseInt(numJugadoresStr);
+                    }
                     // solicito el puntaje minimo para skipear por ronda
                     String puntajeSkipStr = JOptionPane.showInputDialog("Ingrese puntuacion minima para pasar turno: ");
                     int puntajeSkip = Integer.parseInt(puntajeSkipStr);
                     // solicito el puntaje para ganar
-                    String puntajeWinStr = JOptionPane.showInputDialog("Ingrese numero de jugadores: ");
+                    String puntajeWinStr = JOptionPane.showInputDialog("Ingrese el puntaje para ganar: ");
                     int puntajeWin = Integer.parseInt(puntajeWinStr);
                     // declaro e inicializo objeto Farkle
                     Farkle farkle = new Farkle(numJugadores,puntajeSkip,puntajeWin);
@@ -28,6 +40,9 @@ public class Main {
                     break;
                 // caso NO quiere jugar
                 case 1:
+                    JOptionPane.showMessageDialog(null, "Creado por: \nFrancisco Oceguera\nSuffo Peimbert");
+                    break;
+                case 2:
                     break;
             }
         }
